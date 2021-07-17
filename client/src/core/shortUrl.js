b379977c3f4b8553854028b9862ef645843abd68
Venugopal <venugopala11000@gmail.com>
2021-07-17 17:47:02 +0530

    api call for related actions

---
+++ client/src/core/shortUrl.js
@@ -0,0 +1,26 @@
+const apiEndpoint = "http://localhost:5000/";           //process.env.REACT_APP_API_ENDPOINT;   //  Ideally we need to get this from .env
+
+export const getShortUrls = () => {
+    return fetch(apiEndpoint + "shorturl/get")
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
+
+export const createShortUrl = (data) => {
+    console.log(data);
+    return fetch(apiEndpoint + "shorturl/create", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
+
+export const clickShortUrls = (shortUrl) => {
+    return fetch(apiEndpoint + "shorturl/clickUrl/" + shortUrl)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
